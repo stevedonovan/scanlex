3bf1f3d4f03699d675ae0286df5aabfea9ad40b1
Steve Donovan <steve.donovan@panoptix.co.za>
2019-08-17 12:33:28 +0200

    refactor into modules

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use std::{fmt,io};
+use std::error::Error;
+
+/// a scanner error type
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct ScanError {
+    pub details: String,
+    pub lineno: u32,
+}
+
+impl fmt::Display for ScanError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"line {}: {}",self.lineno,self.details)
+    }
+}
+
+impl ScanError {
+    /// create a new error
+    pub fn new(msg: &str) -> ScanError {
+        ScanError{details: msg.into(), lineno: 1}
+    }
+}
+
+impl Error for ScanError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
+impl From<io::Error> for ScanError {
+    fn from(err: io::Error) -> ScanError {
+        ScanError::new(err.description())
+    }
+}
